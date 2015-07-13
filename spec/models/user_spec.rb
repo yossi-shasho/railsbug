a075f1a809238a9305752fb76e40cd7d841364d0
Yossi Shasho <yossi@silo.co>
2015-07-13 16:24:40 +0300

    spec fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe User, type: :model do
   describe '#user_with_own_tasks' do
     it 'should find a user that has a project with a task that has it\'s own email' do
       @task = Task.create! taskable: @project, email: @user.email
-      expect(User.user_with_own_tasks(@user.email)).to == [@user]
+      expect(User.user_with_own_tasks(@user.email)).to eq [@user]
     end
 
   end
